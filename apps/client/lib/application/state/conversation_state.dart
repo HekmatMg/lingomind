8b2d88a763d7fb183f72058f04a6aa2bdf0df875
HekmatMg <hekmatkhah23@gmail.com>
2026-09-15 18:34:18 +0330

    feat(learning): store session corrections in conversation state

--- apps/client/lib/application/state/conversation_state.dart
+++ apps/client/lib/application/state/conversation_state.dart
@@ -1,4 +1,5 @@
 import '../../domain/entities/conversation_message.dart';
+import '../../domain/entities/error_correction.dart';
 import '../../domain/entities/scenario.dart';
 
 class ConversationState {
@@ -6,23 +7,27 @@ class ConversationState {
     required this.scenario,
     required this.messages,
     required this.learnerTurnCount,
+    required this.corrections,
     required this.isFinished,
   });
 
   final Scenario scenario;
   final List<ConversationMessage> messages;
   final int learnerTurnCount;
+  final List<ErrorCorrection> corrections;
   final bool isFinished;
 
   ConversationState copyWith({
     List<ConversationMessage>? messages,
     int? learnerTurnCount,
+    List<ErrorCorrection>? corrections,
     bool? isFinished,
   }) {
     return ConversationState(
       scenario: scenario,
       messages: messages ?? this.messages,
       learnerTurnCount: learnerTurnCount ?? this.learnerTurnCount,
+      corrections: corrections ?? this.corrections,
       isFinished: isFinished ?? this.isFinished,
     );
   }
