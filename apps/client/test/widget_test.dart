cdd5462696c6f12b50009ccd3f04f01c9ad30f78
HekmatMg <hekmatkhah23@gmail.com>
2026-09-15 16:08:43 +0330

    test(client): update home screen coverage

--- apps/client/test/widget_test.dart
+++ apps/client/test/widget_test.dart
@@ -1,12 +1,15 @@
+import 'package:client/main.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:client/main.dart';
-
 void main() {
-  testWidgets('LingoMind app loads', (WidgetTester tester) async {
+  testWidgets('LingoMind home loads learner context', (WidgetTester tester) async {
     await tester.pumpWidget(const LingoMindApp());
 
     expect(find.text('LingoMind'), findsOneWidget);
-    expect(find.text('Welcome to LingoMind'), findsOneWidget);
+    expect(find.text('Welcome back, Learner'), findsOneWidget);
+    expect(find.text('A2'), findsOneWidget);
+    expect(find.text('3 days'), findsOneWidget);
+    expect(find.text('Today\'s recommendation'), findsOneWidget);
+    expect(find.text('At the Coffee Shop'), findsOneWidget);
   });
 }
